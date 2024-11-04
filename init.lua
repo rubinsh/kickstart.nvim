d695ecf10eb8982cda73306bda1c7a68d80dc19a
Shai Rubinstein <rubinsh@gmail.com>
2024-11-04 22:55:43 +0200

    installed fugitive

--- init.lua
+++ init.lua
@@ -234,6 +234,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-fugitive',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   {
     'lewis6991/gitsigns.nvim',
