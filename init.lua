dc402a7bd56a66a817f922fda50815ef959ec4ea
rubinsh <rubinsh@gmail.com>
2024-11-04 23:46:32 +0200

    fixed error with copilot config

--- init.lua
+++ init.lua
@@ -278,7 +278,7 @@ require('lazy').setup({
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'canary',
     dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'github/copilot.vim' }, -- or github/copilot.vim
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
     },
     opts = {
