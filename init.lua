231cb67763519680a909423f4d94f29402c2a338
rubinsh <rubinsh@gmail.com>
2024-12-07 13:03:35 +0200

    added nice LSP diagnostics config, and try to make solargraph work with my rubocop config

--- init.lua
+++ init.lua
@@ -627,6 +627,39 @@ require('lazy').setup({
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
+      vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- Enable underline
+        underline = true,
+        -- Disable virtual text
+        virtual_text = false,
+        -- Enable signs
+        signs = true,
+        -- Disable update in insert mode
+        update_in_insert = false,
+      })
+
+      -- Function to show diagnostics in a floating window
+      local function show_diagnostics()
+        local opts = {
+          focusable = false,
+          close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+          border = 'rounded',
+          source = 'always',
+          prefix = ' ',
+        }
+        vim.diagnostic.open_float(nil, opts)
+      end
+
+      -- Set up an autocommand to show diagnostics on CursorHold
+      vim.api.nvim_create_autocmd('CursorHold', {
+        callback = function()
+          -- Use a timer to delay the execution of the diagnostics popup
+          vim.defer_fn(function()
+            show_diagnostics()
+          end, 1000) -- Delay in milliseconds (1 second)
+        end,
+      })
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -811,6 +844,8 @@ require('lazy').setup({
             symbols = true,
             codeAction = true,
             formatting = true,
+            useBundler = false,
+            checkGemVersion = false,
           },
         },
       }
